d656e23650c4290253d3ec9a692ded1cdaa15128
corazonehealth <info@corazonehealth.com>
2024-11-19 08:55:03 +0200

    Creating structure

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeader } from "../../_componets/pageHeader";
+import { ProductForm } from "../_components/ProductForm";
+
+export default function NewProductPage(){
+    return <>
+        <PageHeader>Add Product</PageHeader>
+        <ProductForm/>
+    </>
+}
